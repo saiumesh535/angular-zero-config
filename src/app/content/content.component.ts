517a6f9861a0f54e078176615aac56eb34dca758
saiumesh535 <d.sai535@gmail.com>
2018-06-10 13:38:50 +0530

    last line in files

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -11,15 +11,9 @@ export class ContentComponent implements OnInit {
   public metadata: string;
   constructor(private route: ActivatedRoute) { }
 
-   code = `function myFunction() {
-  document.getElementById("demo1").innerHTML = "Hello there!";
-  document.getElementById("demo2").innerHTML = "How are you?";
-}`;
-
   ngOnInit() {
     this.route.data.subscribe(lol => {
       this.metadata = lol.metadata;
-      console.log('meta', this.metadata);
     });
   }
 
